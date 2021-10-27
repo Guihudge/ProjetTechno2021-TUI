1061e30f3a7712572afaa31d488195e2d5a4fad4
Guillaume Dindart <guillaume.dindart@etu.u-bordeaux.fr>
2021-10-27 15:07:37 +0200

    Add test source file

---
+++ game_test_gdindart.c
@@ -0,0 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
+
+/*Dummy test*/
+
+bool dummy(){
+    return true;
+}
+
+/*Usage*/
+void usage(int argc, char *argv[]){
+    fprintf(stderr, "Usage : %s <test_name>\n", argv[0]);
+    exit(EXIT_FAILURE);
+}
+
+/*Main*/
+int main(int argc, char *argv[]){
+    if (argc == 1){
+        usage(argc, argv);
+    }
+
+    fprintf(stderr, "Starting test '%s'.\n", argv[1]);
+    bool passed = false;
+    if (strcmp("dummy", argv[1]) == 0){
+        passed = dummy;
+    }
+    else{
+        fprintf(stderr, "Error: unkown test name %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    /*Test result*/
+    if (passed){
+        fprintf(stderr, "Test %s passed\n", argv[1]);
+        exit(EXIT_SUCCESS);
+    }
+    else{
+        fprintf(stderr, "Test %s failed\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+
+}
