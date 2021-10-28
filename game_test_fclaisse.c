dd13fd48100a1bf9615c5f256e0d70b02e68d6e5
Florian Claisse <florian.claisse@etu.u-bordeaux.fr>
2021-10-28 14:25:18 +0200

    add declaration for @fclaisse %1

--- game_test_fclaisse.c
+++ game_test_fclaisse.c
@@ -3,9 +3,60 @@
 #include <stdbool.h>
 #include <string.h>
 
+#include "game.h"
+#include "game_aux.h"
+
 /* ********** TEST DUMMY ********** */
 
 bool test_dummy(void) { return true; }
+
+/* ********** TEST GAME DEFAULT SOLUTION ********** */
+
+bool test_game_default_solution(void) {
+    return true;
+}
+
+/* ********** TEST GAME NEW ********** */
+
+bool test_game_new(void) {
+    return true;
+}
+
+/* ********** TEST GAME SET SQUARE ********** */
+
+bool test_game_set_square(void) {
+    return true;
+}
+
+/* ********** TEST GAME IS LIGHTBULB ********** */
+
+bool test_game_is_lightbulb(void) {
+    return true;
+}
+
+/* ********** TEST GAME IS LIGHTED ********** */
+
+bool test_game_is_lighted(void) {
+    return true;
+}
+
+/* ********** TEST GAME CHECK MOVE ********** */
+
+bool test_game_check_move(void) {
+    return true;
+}
+
+/* ********** TEST GAME PLAY MOVE ********** */
+
+bool test_game_play_move(void) {
+    return true;
+}
+
+/* ********** TEST GAME restart ********** */
+
+bool test_game_restart(void) {
+    return true;
+}
 
 /* ********** USAGE ********** */
 
@@ -25,6 +76,22 @@ int main(int argc, char *argv[]) {
     bool passed = false;
     if(strcmp("dummy", argv[1]) == 0)
         passed = test_dummy();
+    else if(strcmp("game_default_solution", argv[1]) == 0)
+        passed = test_game_default_solution();
+    else if(strcmp("game_new", argv[1]) == 0)
+        passed = test_game_new();
+    else if(strcmp("game_set_square", argv[1]) == 0)
+        passed = test_game_set_square();
+    else if(strcmp("game_is_lightbulb", argv[1]) == 0)
+        passed = test_game_is_lightbulb();
+    else if(strcmp("game_is_lighted", argv[1]) == 0)
+        passed = test_game_is_lighted();
+    else if(strcmp("game_check_move", argv[1]) == 0)
+        passed = test_game_check_move();
+    else if(strcmp("game_play_move", argv[1]) == 0)
+        passed = test_game_play_move();
+    else if(strcmp("game_restart", argv[1]) == 0)
+        passed = test_game_restart();
     else {
         fprintf(stderr, "Error: test \"%s\" not found!\n", argv[1]);
         exit(EXIT_FAILURE);
