ad29bd593a9590ca2027006b996d0629cef077e8
Martin Illhe <martin.illhe@etu.u-bordeaux.fr>
2021-11-05 18:29:43 +0100

    test_game_get_black_number && tes_game_get_square

--- game_test_millhe.c
+++ game_test_millhe.c
@@ -9,6 +9,69 @@ bool test_dummy(){
   return true;
 }
 
+bool test_game_get_black_number(){
+  square test[DEFAULT_SIZE * DEFAULT_SIZE] = {
+    S_LIGHTBULB, S_BLANK, S_BLACK1, S_MARK, S_BLANK, S_BLANK, S_BLANK,
+    S_BLANK, S_BLANK, S_BLACK2, S_BLANK, S_BLANK, S_BLANK, S_BLANK,
+    S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLACKU, S_BLACK2,
+    S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK,
+    S_BLACK1, S_BLACK0, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK,
+    S_BLANK, S_BLANK, S_BLACK4, S_BLANK, S_BLACK3, S_BLANK, S_BLANK,
+    S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLACKU, S_BLANK, S_BLANK};
+
+  game g2 = game_new(test);
+  game_update_flags(g2);
+  int t1 = game_get_black_number(g2, 0, 2);
+  int t2 = game_get_black_number(g2, 1, 2);
+  int t3 = game_get_black_number(g2, 5, 4);
+  int t4 = game_get_black_number(g2, 5, 2);
+  int t = game_get_black_number(g2, 4, 1);
+  int t5 = game_get_black_number(g2, 6, 4);
+  if (t != 0 || t1 != 1 || t2 != 2 || t3 != 3 || t4 != 4 || t5 != -1){
+    return false;
+  }
+  return true;
+}
+ 
+bool test_game_get_square(){
+  square test[DEFAULT_SIZE * DEFAULT_SIZE] = {
+    S_LIGHTBULB, S_BLANK, S_BLACK1, S_MARK, S_BLANK, S_BLANK, S_BLANK,
+    S_BLANK, S_BLANK, S_BLACK2, S_BLANK, S_BLANK, S_BLANK, S_BLANK,
+    S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLACKU, S_BLACK2,
+    S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK,
+    S_BLACK1, S_BLACKU, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK,
+    S_BLANK, S_BLANK, S_BLACK4, S_BLANK, S_BLACK3, S_BLANK, S_BLANK,
+    S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLACKU, S_BLANK, S_BLANK};
+
+  game g2 = game_new(test);
+  game_update_flags(g2);
+  if (game_get_square(g2, 0, 0) != S_LIGHTBULB){
+    return false;
+  }
+  if (game_get_square(g2, 0, 1) != S_BLANK){
+    return false;
+  }
+  if (game_get_square(g2, 0, 2) != S_BLACK1){
+    return false;
+  }
+  if (game_get_square(g2, 0, 3) != S_MARK){
+    return false;
+  }
+  if (game_get_square(g2, 1, 2) != S_BLACK2){
+    return false;
+  }
+  if (game_get_square(g2, 4, 1) != S_BLACKU){
+    return false;
+  }
+  if (game_get_square(g2, 5, 4) != S_BLACK3){
+    return false;
+  }
+  if (game_get_square(g2, 5, 2) != S_BLACK4){
+    return false;
+  }
+  return true;
+}
+
 bool test_game_is_over(){
   square test[DEFAULT_SIZE * DEFAULT_SIZE] = {
     S_BLANK, S_BLANK, S_BLACK1, S_BLANK, S_BLANK, S_BLANK, S_BLANK,
@@ -32,6 +95,12 @@ bool test_game_is_over(){
     };
   game sol_test = game_new(solution_test);
   game_update_flags(sol_test);
+  if(game_is_over(g2)){
+    return false;
+  }
+  if (!game_is_over(sol_test)){
+    return false;
+  }
   for (int i = 0; i < DEFAULT_SIZE; i++)
   {
     for (int j = 0; j < DEFAULT_SIZE; j++)
@@ -39,12 +108,12 @@ bool test_game_is_over(){
       if (game_is_lighbulb(sol_test, i, j))
       {
         game_play_move(g2, i, j, S_LIGHTBULB);
-        if (g2 != sol_test){
+        if (!game_equal(g2, sol_test)){
           if (game_is_over(g2)){
             return false;
           }
         }
-        else if (g2 == sol_test){
+        else if (game_equal(g2, sol_test)){
           return game_is_over(g2);
          }
       }
@@ -55,26 +124,18 @@ bool test_game_is_over(){
 bool test_game_is_blanck(){
 
   square test[DEFAULT_SIZE * DEFAULT_SIZE] = {
-        S_BLANK, S_BLANK, S_BLACK1, S_BLANK, S_BLANK, S_BLANK, S_BLANK,
+        S_LIGHTBULB, S_BLANK, S_BLACK1, S_MARK, S_BLANK, S_BLANK, S_BLANK,
         S_BLANK, S_BLANK, S_BLACK2, S_BLANK, S_BLANK, S_BLANK, S_BLANK,
         S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLACKU, S_BLACK2,
         S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK,
         S_BLACK1, S_BLACKU, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK,
-        S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLACK2, S_BLANK, S_BLANK,
+        S_BLANK, S_BLANK, S_BLACK4, S_BLANK, S_BLACK3, S_BLANK, S_BLANK,
         S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLACKU, S_BLANK, S_BLANK};
   
   game g2 = game_new (test);
-  for (int i = 0; i < DEFAULT_SIZE; i++)
-  {
-    for (int j = 0; j < DEFAULT_SIZE; j++)
-    {
-      char carre = game_get_square(test, i, j);
-      if (! (carre==0) == game_is_blank(test, i, j)){
-        return false;
-      }
-    }
-  }
-  return true;
+  game_update_flags(g2);
+  
+  return !game_is_blank(g2, 0, 0) && game_is_blank(g2, 0, 1) && !game_is_blank(g2, 0, 2) && !game_is_blank(g2, 0, 3) && !game_is_blank(g2, 1, 2) && !game_is_blank(g2, 4, 1) && !game_is_blank(g2, 5, 2) && !game_is_blank(g2, 5, 4);
   
 }
 bool test_game_delete(){
@@ -88,27 +149,29 @@ bool test_game_delete(){
       S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLACKU, S_BLANK, S_BLANK};
     game g2 = game_new(test);
     game_delete(g2);
-    if (g2!=NULL)
-    {
-      return false;
-    }
-    return true;
+    return (g2==NULL);
 }
 
 bool test_game_copy(){
   square test[DEFAULT_SIZE * DEFAULT_SIZE] = {
-      S_BLANK, S_BLANK, S_BLACK1, S_BLANK, S_BLANK, S_BLANK, S_BLANK,
+      S_LIGHTBULB, S_LIGHTBULB, S_BLACK1, S_MARK, S_BLANK, S_BLANK, S_BLANK,
       S_BLANK, S_BLANK, S_BLACK2, S_BLANK, S_BLANK, S_BLANK, S_BLANK,
-      S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLACKU, S_BLACK2,
+      S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLACK0, S_BLACK2,
       S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK,
-      S_BLACK1, S_BLACKU, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK,
-      S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLACK2, S_BLANK, S_BLANK,
+      S_BLACK3, S_BLACKU, S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLANK,
+      S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLACK4, S_BLANK, S_BLANK,
       S_BLANK, S_BLANK, S_BLANK, S_BLANK, S_BLACKU, S_BLANK, S_BLANK};
   
   game g2 = game_new(test);
   game g3 = game_copy(g2);
-  return game_equal(g3, test);
+  if (!game_equal(g3, test)){
+    return false;
+  }
+  game_update_flags(g2);
+  g3 = game_copy(g2);
+  return game_equal(g2, g3);
 }
+
 void usage(int argc, char *argv[])
 {
   fprintf(stderr, "Usage: %s <testname> [<...>]\n", argv[0]);
