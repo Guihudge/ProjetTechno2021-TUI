bca255c63e9c9b5fa0164d2d8e2faae02ebc657c
Florian Claisse <florian.claisse@etu.u-bordeaux.fr>
2021-11-29 18:00:32 +0100

    Add auxiliars function in auxiliars.c

---
+++ auxiliars.c
@@ -0,0 +1,46 @@
+#include "auxiliars.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void memory_error(void) {
+    fprintf(stderr, "Memory error\n");
+    exit(EXIT_FAILURE);
+}
+
+void pointer_error(void) {
+    fprintf(stderr, "Pointeur error\n");
+    exit(EXIT_FAILURE);
+}
+
+void is_viable_pointer(const void *pointer, const char *type) {
+    if (!pointer) {
+        if (strcmp("memory", type) == 0)
+            memory_error();
+        else if (strcmp("pointer", type) == 0)
+            pointer_error();
+        else {
+            fprintf(stderr, "Unknown error type by fonction %s\n", __func__);
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+void check_coordinates(cgame g, uint i, uint j, const char *fonction) {
+    if (i >= DEFAULT_SIZE || j >= DEFAULT_SIZE) {
+        fprintf(stderr, "Wrong arguments (%d,%d) for (%d,%d) inside %s call\n", i, j, DEFAULT_SIZE, DEFAULT_SIZE,
+                fonction);
+        exit(EXIT_FAILURE);
+    }
+}
+
+bool delete_and_exit(bool exit, game *games, uint size, char *error) {
+    for (uint i = 0; i != size; i++) {
+        game_delete(games[i]);
+    }
+    if (strcmp(error, "")) {
+        fprintf(stderr, "%s\n", error);
+    }
+    return exit;
+}
---
+++ auxiliars.h
@@ -0,0 +1,57 @@
+#ifndef __AUXILIARS_H__
+#define __AUXILIARS_H__
+
+#include <stdbool.h>
+
+#include "game.h"
+
+/**
+ * @brief Prints an error message and exit the program in connection with memory allocation error.
+ * @deprecated Ne pas utiliser dans les test
+ **/
+void memory_error();
+
+/**
+ * @brief Prints an error message and exit the program in connection with memory pointer error.
+ * @deprecated Ne pas utiliser dans les test
+ **/
+void pointer_error();
+
+/**
+ * @brief Check viability of a pointer and exit the program with the error message associated
+ * in case where it would be unvalid.
+ * @param pointer Tab pointer that must be checked.
+ * @param type Inform of the potential error type.
+ * @pre @p pointer Must be a pointer tab with the potential error type associated.
+ * @pre @p type Must be exactly "pointer" or "memory".
+ * @deprecated Ne pas utiliser dans les test
+ **/
+void is_viable_pointer(const void *pointer, const char *type);
+
+/**
+ * @brief Check that coordinates are valid for the game and exit the program in case of error.
+ * @param g The game.
+ * @param i Row index.
+ * @param j Column index.
+ * @param fonction The string taht correspond to the fonction where check_coordinates been called.
+ * @pre @p g Must be a valid pointer toward a game structure.
+ * @pre @p fonction Must correspond to the fonction.
+ * @deprecated Ne pas utiliser dans les test
+ **/
+void check_coordinates(cgame g, uint i, uint j, const char *fonction);
+
+/**
+ * @brief Deletes games in a tab of a given size and prints an error message.
+ * @param exit Given boolean indicating in the exit of a boolean function should be true of false.
+ * @param games Tab filled by games to delete pointers.
+ * @param size The size of the games pointer tab.
+ * @param error Error message associated.
+ * @pre @p games Pointers should be valid and their number and location in the
+ * linear tab should correspond to the games that should be deleted, other pointers
+ * in the tab located after games[size-1] would be ignored.
+ * @pre @p erre Can be set to "" if no error message is needed.
+ * @return The given boolean.
+ **/
+bool delete_and_exit(bool exit, game *games, uint size, char *error);
+
+#endif
